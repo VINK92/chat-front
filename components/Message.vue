06a0cd31ae5400a3954d7a5d09c7c33533fefec4
VINK92 <66158210+VINK92@users.noreply.github.com>
2022-04-22 17:53:03 +0300

    `add updates`

---
+++ components/Message.vue
@@ -0,0 +1,38 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="messageContainer">
+    <span>{{messageTime}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+import moment from 'moment';
+export default Vue.extend({
+  props() {
+    return {
+      variant: {
+        type: String,
+        dafault: 'grey',
+      }
+    }
+  },
+  data() {
+    return {
+      messageTime: moment().format('LT')
+    }
+  }
+})
+
+</script>
+
+<style scoped>
+.messageContainer {
+  width: 800px;
+  height: 100px;
+  background: rgb(243, 242, 242);
+  box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+  border-radius: 5px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="container">
+      <img :src="srcTitleImg" alt="Patric" width="300" height="300">
+      <div class="titleContainer">
+        <h1 class="title">{{title}}</h1>
+        <p>{{subTitle}}</p>
+      </div>
+    </div>
+    <Message/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Chat bots 2.0',
+      subTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      srcTitleImg: '/static/images/bigchatbot.jpg',
+    }
+  }
+})
+</script>
+
+<style>
+#__nuxt {
+  font-family: "Open Sans";
+  font-size: 14px;
+}
+.container {
+  display: flex;
+  flex-direction: row;
+}
+.titleContainer {
+  display: flex;
+  flex-direction: column;
+}
+.title {
+  color: #333333;
+}
+</style>
